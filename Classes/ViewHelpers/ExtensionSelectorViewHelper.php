c17ed6d2922f2da21cfac59796564073112d992b
Nicole Cordes <n.cordes@biz-design.biz>
2012-07-21 20:49:10 +0200

    [TASK] Move creating the extension drop-down to a ViewHelper
    Change-Id: Id6231fcdf90a852dc9d1d3d53d772c2bb94cda97
    Resolves: #36104

---
+++ Classes/ViewHelpers/ExtensionSelectorViewHelper.php
@@ -0,0 +1,145 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ * (c) 2012 Nicole Cordes <nicole.cordes@googlemail.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * This view helper renders the extension selector box.
+ *
+ * @package TYPO3
+ * @subpackage tx_phpunit
+ *
+ * @author Nicole Cordes <nicole.cordes@googlemail.com>
+ */
+class Tx_Phpunit_ViewHelpers_ExtensionSelectorViewHelper extends Tx_Phpunit_ViewHelpers_AbstractSelectorViewHelper {
+	/**
+	 * Gets all options rendered as an array
+	 *
+	 * @return array<string> all options as html string provided in an indexed array, might be empty
+	 */
+	protected function getOptions() {
+		$options = array();
+
+			// Render option tag for "all extensions"
+		$parameterArray = array(
+			'class' => 'alltests',
+			'value' => 'uuall'
+		);
+		if ($this->userSettingService->getAsString(Tx_Phpunit_Interface_Request::PARAMETER_KEY_TESTABLE) === Tx_Phpunit_Testable::ALL_EXTENSIONS) {
+			$parameterArray['selected'] = 'selected';
+		}
+		$options[] = $this->renderTag('option', $parameterArray, $GLOBALS['LANG']->getLL('all_extensions'));
+
+			// Render option tags for each extension
+		$testables = $this->testFinder->getTestablesForEverything();
+		/** @var $testable Tx_Phpunit_Testable */
+		foreach ($testables as $testable) {
+			$parameterArray = array(
+				'style' => $this->createIconStyle($testable->getKey(), FALSE),
+				'value' => htmlspecialchars($testable->getKey())
+			);
+			if ($this->userSettingService->getAsString(Tx_Phpunit_Interface_Request::PARAMETER_KEY_TESTABLE) === $testable->getKey()) {
+				$parameterArray['selected'] = 'selected';
+			}
+
+			$options[] = $this->renderTag('option', $parameterArray, htmlspecialchars($testable->getKey()));
+		}
+
+		return $options;
+	}
+	/**
+	 * Renders the content of the viewhelper and pushes it to the output service.
+	 *
+	 * @return void
+	 */
+	public function render() {
+			// Get all options
+		$options = $this->getOptions();
+
+		$selectedExtension = '';
+		foreach ($options as $option) {
+			if (strpos($option, 'selected="selected"') !== FALSE) {
+				preg_match('/<option[^>]*value="([^"]*)"[^>]*>/', $option, $matches);
+				$selectedExtension = $matches[1];
+				break;
+			}
+		}
+		if ($selectedExtension == Tx_Phpunit_Testable::ALL_EXTENSIONS) {
+			$selectedExtension = '';
+		}
+
+		$content = $this->renderTag(
+			'select',
+			array(
+				'name' => Tx_Phpunit_Interface_Request::PARAMETER_NAMESPACE .
+					'[' .Tx_Phpunit_Interface_Request::PARAMETER_KEY_TESTABLE . ']',
+				'onchange' => 'jumpToUrl(\'' . $this->action . '&' .
+					Tx_Phpunit_Interface_Request::PARAMETER_NAMESPACE .
+					'[' . Tx_Phpunit_Interface_Request::PARAMETER_KEY_TESTABLE . ']=' .
+					'\'+this.options[this.selectedIndex].value,this);',
+				'style' => ($selectedExtension !== '') ? $this->createIconStyle($selectedExtension, FALSE) : ''
+			),
+			implode('', $options)
+		);
+
+			// Add button tag
+		$content .= $this->renderTag(
+			'button',
+			array(
+				'accesskey' => 'a',
+				'name' => Tx_Phpunit_Interface_Request::PARAMETER_NAMESPACE .
+					'[' . Tx_Phpunit_Interface_Request::PARAMETER_KEY_EXECUTE . ']',
+				'type' => 'submit',
+				'value' => 'run'
+			),
+			$GLOBALS['LANG']->getLL('run_all_tests')
+		);
+
+			// Add hidden field
+		$content .= $this->renderTag(
+			'input',
+			array(
+				'name' => Tx_Phpunit_Interface_Request::PARAMETER_NAMESPACE .
+					'[' . Tx_Phpunit_Interface_Request::PARAMETER_KEY_COMMAND . ']',
+				'type' => 'hidden',
+				'value' => 'runalltests'
+			)
+		);
+
+			// Surround with paragraph
+		$content = $this->renderTag('p', array(), $content);
+
+			// Surround with form
+		$content = $this->renderTag(
+			'form',
+			array(
+				'action' => htmlspecialchars($this->action),
+				'method' => 'post'
+			),
+			$content
+		);
+
+		$this->outputService->output($content);
+	}
+}
+?>
