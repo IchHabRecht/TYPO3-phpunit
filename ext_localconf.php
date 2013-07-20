8132a954b01a204156fd76782910949f55f9ea71
Nicole Cordes <typo3@cordes.co>
2013-07-21 00:44:46 +0200

    [BUGFIX] Fatal error when phpunit-selenium is not installed
    Fatal error: Class 'PHPUnit_Extensions_Selenium2TestCase' not found when
    running the tests from the command line and when Selenium is not
    installed.
    
    Change-Id: I685083c68dd6e285eccc7ea9854df3cae32386b7
    Resolves: #50230

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,26 +3,32 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-require_once(t3lib_extMgm::extPath('phpunit') . 'Composer/vendor/autoload.php');
-require_once(t3lib_extMgm::extPath('phpunit') . 'Migrations/vfsStream.php');
-
 /** @var $extensionSettingsService Tx_Phpunit_Service_ExtensionSettingsService */
 $extensionSettingsService = t3lib_div::makeInstance('Tx_Phpunit_Service_ExtensionSettingsService');
 $composerPhpUnitPath = t3lib_extMgm::extPath('phpunit') . 'Composer/vendor/phpunit/phpunit/';
 if ($extensionSettingsService->hasString('phpunitlib')) {
 	$userPhpUnitPath = rtrim(t3lib_div::fixWindowsFilePath($extensionSettingsService->getAsString('phpunitlib')), '/');
 	if (is_dir($userPhpUnitPath . '/PHPUnit')) {
-		$composerPhpUnitPath = $userPhpUnitPath . '/';
+		if (is_file($userPhpUnitPath . '/PHPUnit/Autoload.php')) {
+			$composerPhpUnitPath = $userPhpUnitPath . '/';
+			if (set_include_path($composerPhpUnitPath . PATH_SEPARATOR . get_include_path()) !== FALSE) {
+				require_once($composerPhpUnitPath . 'PHPUnit/Autoload.php');
+				spl_autoload_unregister('phpunit_autoload');
+			}
+		}
 	}
 }
 unset($extensionSettingsService);
 
 define(TX_PHPUNITLIB_EXTPATH, $composerPhpUnitPath);
-set_include_path(TX_PHPUNITLIB_EXTPATH . PATH_SEPARATOR . get_include_path());
 unset($composerPhpUnitPath);
 
-$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['Tx_Phpunit_BackEnd_Ajax']
-	= 'typo3conf/ext/phpunit/Classes/BackEnd/Ajax.php:Tx_Phpunit_BackEnd_Ajax->ajaxBroker';
+require_once(t3lib_extMgm::extPath('phpunit') . 'Composer/vendor/autoload.php');
+require_once(t3lib_extMgm::extPath('phpunit') . 'Migrations/vfsStream.php');
+spl_autoload_register('phpunit_autoload', TRUE, TRUE);
+
+$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['Tx_Phpunit_BackEnd_Ajax'] =
+	'typo3conf/ext/phpunit/Classes/BackEnd/Ajax.php:Tx_Phpunit_BackEnd_Ajax->ajaxBroker';
 
 if (TYPO3_MODE === 'BE') {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys']['phpunit'] = array(
