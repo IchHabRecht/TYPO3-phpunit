c39ae1268b465ccefd06f389ee11bd6a3e918f15
Stefano Kowalke <blueduck@gmx.net>
2014-01-20 12:18:38 +0100

    [BUGFIX] Helper functions break connection to current database
    Calling createDatabase(), cleanDatabase() or dropDatabase() breaks the
    database connection which is defined in
    $GLOBALS['TYPO3_CONF_VARS']['DB']['database‘]. This patch reconnects to
    this database within these methods.
    
    Change-Id: I72f9c14bea7b839e13472bd112fd887134f2e020
    Resolves: #55144
    Releases: 3.7, 3.8

--- Classes/Database/TestCase.php
+++ Classes/Database/TestCase.php
@@ -80,6 +80,7 @@ abstract class Tx_Phpunit_Database_TestCase extends Tx_Phpunit_TestCase {
 		/** @var $db t3lib_DB */
 		$db = $GLOBALS['TYPO3_DB'];
 		$databaseNames = $db->admin_get_dbs();
+		$this->selectDatabase($GLOBALS['TYPO3_CONF_VARS']['DB']['database'], $db);
 
 		if (!in_array($this->testDatabase, $databaseNames)) {
 			if ($db->admin_query('CREATE DATABASE `' . $this->testDatabase . '`') === FALSE) {
@@ -98,7 +99,10 @@ abstract class Tx_Phpunit_Database_TestCase extends Tx_Phpunit_TestCase {
 	protected function cleanDatabase() {
 		/** @var $db t3lib_DB */
 		$db = $GLOBALS['TYPO3_DB'];
-		if (!in_array($this->testDatabase, $db->admin_get_dbs())) {
+		$databaseNames = $db->admin_get_dbs();
+		$this->selectDatabase($GLOBALS['TYPO3_CONF_VARS']['DB']['database'], $db);
+
+		if (!in_array($this->testDatabase, $databaseNames)) {
 			return;
 		}
 
@@ -119,7 +123,10 @@ abstract class Tx_Phpunit_Database_TestCase extends Tx_Phpunit_TestCase {
 	protected function dropDatabase() {
 		/** @var $db t3lib_DB */
 		$db = $GLOBALS['TYPO3_DB'];
-		if (!in_array($this->testDatabase, $db->admin_get_dbs())) {
+		$databaseNames = $db->admin_get_dbs();
+		$this->selectDatabase($GLOBALS['TYPO3_CONF_VARS']['DB']['database'], $db);
+
+		if (!in_array($this->testDatabase, $databaseNames)) {
 			return TRUE;
 		}
 
