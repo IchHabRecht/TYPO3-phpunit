1c5837265983cd14e014306d44026774e9358b1c
wukitech <wukitech@gmx.net>
2015-01-01 21:12:48 +0100

    [Feature] Read phpunit.xml to get the WhiteList for the CodeCoverage
    Added missing XmlConfigurationService.php file
    
    New Service is introduced to read in xml configuration from the
    phpunit.xml file. Currently the new Service is supporting only
    whitelist and blacklist configuration.
    The BackEnd/Module.php is patched to fetch the xml configuration
    and set accroding the xml settings the correct
    PHP_CodeCoverage_Filter. Open is that if no phpunit.xml file
    can be fount inside the test folder of the object under test an
    not handled exception can be used to set some default filters.
    
    Change-Id: I8362edf528c0bcec650885f7b5e437b006a26191
    Resolves: #64056
    Related: #64056

---
+++ Classes/Service/XmlConfigurationService.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\SingletonInterface;
+
+/**
+ * This class provides functions for reading and writing the settings of the back-end user who is currently logged in.
+ *
+ * This class may only be used when a back-end user is logged in.
+ *
+ * @package TYPO3
+ * @subpackage tx_phpunit
+ *
+ * @author Robert Wukitsevits <wukitech@gmx.net>
+ */
+class Tx_Phpunit_Service_XmlConfigurationService implements SingletonInterface
+{
+	/**
+	 * @var string
+	 */
+	const PHPUNIT_XML_CONFIGURATION_FILE = 'phpunit.xml';
+        
+        /**
+         * @var SimpleXMLElement
+         */
+        protected $xmlPhpunit=NULL;
+
+
+        /**
+         * Load the xml configuration file with the name phpunit.xml
+         * 
+         * Details for the structure of the xml file can be found at 
+         * https://phpunit.de/manual/current/en/appendixes.configuration.html
+         * 
+         * @param string $path
+         * @throws Exception 1418933134 There have to be a file like phpunit.xml
+         */
+        public function loadFile($path=NULL) {
+            $file = $path . self::PHPUNIT_XML_CONFIGURATION_FILE;
+            if (file_exists($file)) {
+                $this->xmlPhpunit = simplexml_load_file($file);
+            } else {
+                throw new Exception('No xml cofiguration file found (file:' . $file . ').', 1418933134);
+            }
+        }
+        
+        /**
+         * Extract the black list from the phpunit xml configuration file
+         * 
+         * @return array strings with the path/file names
+         */
+        public function getWhitelist() {
+            $list = array();
+            
+            foreach($this->xmlPhpunit->filter->whitelist->directory as $directory) {
+                switch((string) $directory['suffix']) { // Get attributes as element indices
+                    case '.php':
+                        array_push($list, (string)$directory);
+                        break;
+                    default:
+                        array_push($list, (string)$directory);
+                        break;
+                    }
+            }
+            
+            foreach($this->xmlPhpunit->filter->whitelist->file as $file) {
+                array_push($list, (string)$file);
+            }
+            
+            return $list;
+        }
+        
+        /**
+         * Extract the black list from the phpunit xml configuration file
+         * 
+         * @remark currently the blacklist feature is not supported form class PHP_CodeCoverage_Filter
+         * @return array strings with the path/file names
+         */
+        public function getBlacklist() {
+            $list = array();
+            
+            foreach($this->xmlPhpunit->filter->blacklist->directory as $directory) {
+                switch((string) $directory['suffix']) { // Get attributes as element indices
+                    case '.php':
+                        array_push($list, (string)$directory);
+                        break;
+                    default:
+                        array_push($list, (string)$directory);
+                        break;
+                    }
+            }
+            
+            foreach($this->xmlPhpunit->filter->blacklist->file as $file) {
+                array_push($list, (string)$file);
+            }
+            
+            return $list;
+        }       
+        
+}
